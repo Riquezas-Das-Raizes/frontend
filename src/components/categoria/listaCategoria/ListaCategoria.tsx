f58f70d0aee481425f4eebaef848d74a8ae24731
Vinicios Gabriel <vini.webdeveloper@gmail.com>
2024-06-03 11:13:28 -0300

    Get Categoria

---
+++ src/components/categoria/listaCategoria/ListaCategoria.tsx
@@ -0,0 +1,51 @@
+import { useContext, useEffect, useState } from "react";
+import Categoria from "../../../models/Categoria";
+import { buscar } from "../../../services/Service";
+import { AuthContext } from "../../../contexts/AuthContext";
+import CardCategoria from "../cardTema/CardCategoria";
+// import { useNavigate } from "react-router-dom"; // Uncomment if navigation is needed
+
+function ListaCategoria() {
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
+  const { usuario, handleLogout } = useContext(AuthContext);
+  const token = usuario.token;
+  // let navigate = useNavigate();
+
+  console.log(categorias);
+
+  async function listar() {
+    try {
+      await buscar("/categorias", setCategorias, {
+        Authorization: token,
+      });
+    } catch (error: any) {
+      if (error.toString().includes("403")) {
+        alert("O token expirou, favor logar novamente");
+        handleLogout();
+      }
+    }
+  }
+
+  // useEffect(() => {
+  //   if (token === "") {
+  //     alert("Você precisa estar logado");
+  //     // navigate("/login"); // Uncomment if navigation is needed
+  //   } else {
+  //     listar();
+  //   }
+  // }, [token]);
+
+  useEffect(() => {
+    listar();
+  }, [categorias.length]);
+
+  return (
+    <>
+      {categorias.map((categoria) => (
+        <CardCategoria key={categoria.id} categoria={categoria} />
+      ))}
+    </>
+  );
+}
+
+export default ListaCategoria;
