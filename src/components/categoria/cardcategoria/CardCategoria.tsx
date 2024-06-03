84c6d594dd26ad97deeb0e62fecd9a1a52337542
Gabriel-Aranda1406 <biel.aranda234@gmail.com>
2024-06-03 14:37:07 -0300

    fix: Estilização botões editar e deletar categoria

--- src/components/categoria/cardcategoria/CardCategoria.tsx
+++ src/components/categoria/cardcategoria/CardCategoria.tsx
@@ -13,7 +13,7 @@ function CardCategoria({ categoria }: CardCategoriaProps) {
 
   return (
     <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
-      <header className="py-2 px-6 bg-[rgb(218,20,0)] text-white font-bold text-2xl">
+      <header className="py-2 px-6 bg-[#ADC178] text-white font-bold text-2xl">
         Categoria
       </header>
       <p className="p-8 text-3xl bg-slate-200 h-full">{categoria.nome}</p>
@@ -21,7 +21,7 @@ function CardCategoria({ categoria }: CardCategoriaProps) {
         <div className="flex">
           <Link
             to={`/editarcategoria/${categoria.id}`}
-            className="w-full text-slate-100 bg-[rgb(254,156,0)] hover:bg-[rgb(254,184,69)]
+            className="w-full text-slate-100 bg-yellow-700 hover:bg-yellow-900
                         flex items-center justify-center py-2"
           >
             <button>Editar</button>
@@ -29,7 +29,7 @@ function CardCategoria({ categoria }: CardCategoriaProps) {
 
           <Link
             to={`/deletarcategoria/${categoria.id}`}
-            className="text-slate-100 bg-red-900 hover:bg-[rgb(218,20,0)] w-full 
+            className="text-slate-100 bg-red-700 hover:bg-[rgb(218,20,0)] w-full 
                         flex items-center justify-center"
           >
             <button>Deletar</button>
