7468d75bc98c05d88b004ba002490f2953c88ae8
Lorena Belo <loore.belo@gmail.com>
2024-06-04 15:44:43 -0300

    feat: adicionado estrutura na navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,13 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
+import Categoria from "../../models/Categoria";
+import { ShoppingBag, SignOut, User } from "@phosphor-icons/react";
 
 function Navbar() {
   const navigate = useNavigate();
-
   const { usuario, handleLogout } = useContext(AuthContext);
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
 
   function logout() {
     handleLogout();
@@ -13,43 +15,69 @@ function Navbar() {
     navigate("/");
   }
 
+  useEffect(() => {
+    async function listar() {
+      try {
+        await buscarCat("/categorias", setCategorias);
+      } catch (error) {
+        console.error("Erro ao buscar categorias:", error);
+      }
+    }
+    listar();
+  }, []);
+
   return (
     <>
-      <nav className="bg-custom-bg py-4 px-6 flex justify-between items-center">
-        <Link to="/" className="flex items-center">
-          <img
-            src="src\assets\img\logo.png"
-            alt="Logo"
-            className="w-16 h-auto mr-2"
-          />
-          <span className="text-custom-text font-semibold">
-            Riqueza das Raízes
-          </span>
-        </Link>
-        <div className="flex space-x-6 text-custom-text">
+      <nav className="bg-custom-green shadow-xl py-5 px-6 flex-col justify-center items-center rounded-full mx-20 mt-20 relative">
+        <div className="flex justify-center pt-10">
+          <Link to="/" className="absolute inset-x-0 top-1/2 transform -translate-y-full flex justify-center items-center">
+            <img
+              src="src/assets/img/logo.png"
+              alt="Logo"
+              className="w-52 h-auto"
+            />
+          </Link>
+        </div>
+        <div className="flex justify-center items-end space-x-6 text-white">
           <Link to="/" className="hover:underline">
             Home
           </Link>
-          <div className="hover:underline">Produtos</div>
+          {categorias.map((categoria) => (
+            <Link key={categoria.id} to={`/categoria/${categoria.id}`} className="hover:underline">
+              {categoria.nome}
+            </Link>
+          ))}
           <Link to="/sobrenos" className="hover:underline">
             Sobre Nós
           </Link>
-          <Link to="/contatos" className="hover:underline">
-            Contatos
+          <Link to="#" className="hover:underline">
+            Artesões
           </Link>
-          <Link to="/categorias" className="hover:underline">
-            Categorias
-          </Link>
-          <Link to="/cadastrarCategoria" className="hover:underline">Cadastrar Categoria</Link>
+          {usuario.admin && (
+            <>
+              <Link to="/produtos" className="hover:underline">
+                Produtos
+              </Link>
+              <Link to="/categorias" className="hover:underline">
+                Categorias
+              </Link>
+              <Link to="/cadastrarCategoria" className="hover:underline">
+                Cadastrar Categoria
+              </Link>
+            </>
+          )}
           {usuario.token ? (
             <Link to="" onClick={logout} className="hover:underline">
-              Sair
+              <SignOut size={25} />
             </Link>
           ) : (
-            <Link to="/login" className="hover:underline">
-              Faça login
+            <Link to="/Login" className="hover:underline">
+              <User size={25} />
             </Link>
           )}
+          <Link to="#" className="hover:underline">
+            <ShoppingBag size={25} />
+          </Link>
         </div>
       </nav>
     </>
