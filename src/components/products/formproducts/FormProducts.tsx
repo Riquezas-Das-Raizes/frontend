eaa0d2e1f50bdcb790e21de912bb098f1b6c6646
Lorena Belo <loore.belo@gmail.com>
2024-06-05 16:24:54 -0300

    fix: ajustes gerais

--- src/components/products/formproducts/FormProducts.tsx
+++ src/components/products/formproducts/FormProducts.tsx
@@ -8,36 +8,33 @@ import { buscar, atualizar, cadastrarProd, buscarCat } from "../../../services/S
 import { hotAlerta } from "../../../util/hotAlerta";
 
 function FormProduct() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
+  const [categoria, setCategoria] = useState<Categoria>({ id: 0, nome: '' });
+  const [product, setProduct] = useState<Product>({
+    id: 0,
+    nome: '',
+    preco: 0,
+    imagem: '',
+    descricao: '',
+    categoria: null,
+    usuario: null,
+  });
 
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [categorias, setCategorias] = useState<Categoria[]>([]);
+  const { id } = useParams<{ id: string }>();
+  const { usuario, handleLogout } = useContext(AuthContext);
+  const token = usuario.token;
 
-    const [categoria, setCategoria] = useState<Categoria>({ id: 0, nome: '' });
-    const [product, setProduct] = useState<Product>({
-        id: 0,
-        nome: '',
-        preco: 0,
-        imagem: '',
-        descricao: '',
-        categoria: null,
-        usuario: null,
-    });
-
-    const { id } = useParams<{ id: string }>();
-    const { usuario, handleLogout } = useContext(AuthContext);
-    const token = usuario.token;
-
-    async function buscarProductPorId(id: string) {
-        try {
-            await buscar(`/produtos/${id}`, setProduct, {
-                headers: { Authorization: token }
-            });
-        } catch (error: any) {
-            if (error.toString().includes('403')) {
-                handleLogout();
-            }
-        }
+  async function buscarProductPorId(id: string) {
+    try {
+      await buscar(`/produtos/${id}`, setProduct, {
+        headers: { Authorization: token }
+      });
+    } catch (error: any) {
+      if (error.toString().includes('403')) {
+        handleLogout();
+      }
     }
   }
 
@@ -63,7 +60,7 @@ function FormProduct() {
     if (token === "") {
       navigate("/");
     }
-  }, [token]);
+  }, [token, navigate]);
 
   useEffect(() => {
     buscarCategorias();
@@ -91,44 +88,28 @@ function FormProduct() {
     navigate("/produtos");
   }
 
-    async function gerarNovaProduct(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault();
-        setIsLoading(true);
-    
-        const productData = {
-            ...product,
-            categoria: categoria,
-            usuario: usuario,
-        };
-    
-        if (id !== undefined) {
-            try {
-                await atualizar(`/produtos`, productData, setProduct, {
-                    headers: {
-                        Authorization: token,
-                    },
-                });
-                hotAlerta("Produto atualizado com sucesso", 'sucesso');
-            } catch (error: any) {
-                hotAlerta("Erro ao atualizar o produto:", 'erro');
-                if (error.toString().includes('403')) {
-                    handleLogout();
-                }
-            }
-        } else {
-            try {
-                await cadastrarProd(`/produtos`, productData, setProduct, {
-                    headers: {
-                        Authorization: token,
-                    },
-                });
-                hotAlerta("Produto cadastrado com sucesso", 'sucesso');
-            } catch (error: any) {
-                hotAlerta("Erro ao cadastrar o produto", 'erro');
-                if (error.toString().includes('403')) {
-                    handleLogout();
-                }
-            }
+  async function gerarNovaProduct(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const productData = {
+      ...product,
+      categoria: categoria,
+      usuario: usuario,
+    };
+
+    if (id !== undefined) {
+      try {
+        await atualizar(`/produtos`, productData, setProduct, {
+          headers: {
+            Authorization: token,
+          },
+        });
+        hotAlerta("Produto atualizado com sucesso", 'sucesso');
+      } catch (error: any) {
+        hotAlerta("Erro ao atualizar o produto:", 'erro');
+        if (error.toString().includes('403')) {
+          handleLogout();
         }
       }
     } else {
@@ -138,10 +119,10 @@ function FormProduct() {
             Authorization: token,
           },
         });
-        alert("Produto cadastrado com sucesso");
+        hotAlerta("Produto cadastrado com sucesso", 'sucesso');
       } catch (error: any) {
-        alert("Erro ao cadastrar o produto");
-        if (error.toString().includes("403")) {
+        hotAlerta("Erro ao cadastrar o produto", 'erro');
+        if (error.toString().includes('403')) {
           handleLogout();
         }
       }
@@ -159,87 +140,18 @@ function FormProduct() {
         {id !== undefined ? "Editar Produto" : "Cadastrar Produto"}
       </h1>
 
-            <form className="flex flex-col w-1/2 gap-4" onSubmit={gerarNovaProduct}>
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="nome">Nome do Produto</label>
-                    <input
-                        type="text"
-                        placeholder="Nome"
-                        name="nome"
-                        required
-                        className="border-2 border-slate-700 rounded p-2"
-                        value={product.nome}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                </div>
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="preco">Preço</label>
-                    <input
-                        type="number"
-                        placeholder="Preço"
-                        name="preco"
-                        required
-                        className="border-2 border-slate-700 rounded p-2"
-                        value={product.preco}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                </div>
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="imagem">Imagem</label>
-                    <input
-                        type="text"
-                        placeholder="Imagem URL"
-                        name="imagem"
-                        required
-                        className="border-2 border-slate-700 rounded p-2"
-                        value={product.imagem}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                </div>
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="descricao">Descrição</label>
-                    <input
-                        type="text"
-                        placeholder="Descrição"
-                        name="descricao"
-                        required
-                        className="border-2 border-slate-700 rounded p-2"
-                        value={product.descricao}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                    />
-                </div>
-                <div className="flex flex-col gap-2">
-                    <p>Qual a categoria do produto?</p>
-                    <select
-                        name="categoria"
-                        id="categoria"
-                        className='border p-2 border-slate-800 rounded'
-                        onChange={(e) => setCategoria(categorias.find(cat => cat.id === parseInt(e.currentTarget.value)) || { id: 0, nome: '' })}
-                    >
-                        <option value="" disabled selected>Selecione uma Categoria</option>
-                        {categorias.map((categoria) => (
-                            <option key={categoria.id} value={categoria.id}>{categoria.nome}</option>
-                        ))}
-                    </select>
-                </div>
-                <button
-                    type='submit'
-                    className='rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800
-                               text-white font-bold w-1/2 mx-auto py-2 flex justify-center'
-                    disabled={carregandoCategoria}
-                >
-                    {isLoading ?
-                        <RotatingLines
-                            strokeColor="white"
-                            strokeWidth="5"
-                            animationDuration="0.75"
-                            width="24"
-                            visible={true}
-                        /> :
-                        <span>{id !== undefined ? 'Atualizar' : 'Cadastrar'}</span>
-                    }
-                </button>
-            </form>
+      <form className="flex flex-col w-1/2 gap-4" onSubmit={gerarNovaProduct}>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="nome">Nome do Produto</label>
+          <input
+            type="text"
+            placeholder="Nome"
+            name="nome"
+            required
+            className="border-2 border-slate-700 rounded p-2"
+            value={product.nome}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+          />
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="preco">Preço</label>
@@ -282,42 +194,31 @@ function FormProduct() {
           <select
             name="categoria"
             id="categoria"
-            className="border p-2 border-slate-800 rounded"
-            onChange={(e) =>
-              setCategoria(
-                categorias.find(
-                  (cat) => cat.id === parseInt(e.currentTarget.value),
-                ) || { id: 0, nome: "" },
-              )
-            }
+            className='border p-2 border-slate-800 rounded'
+            onChange={(e) => setCategoria(categorias.find(cat => cat.id === parseInt(e.currentTarget.value)) || { id: 0, nome: '' })}
           >
-            <option value="" disabled selected>
-              Selecione uma Categoria
-            </option>
+            <option value="" disabled selected>Selecione uma Categoria</option>
             {categorias.map((categoria) => (
-              <option key={categoria.id} value={categoria.id}>
-                {categoria.nome}
-              </option>
+              <option key={categoria.id} value={categoria.id}>{categoria.nome}</option>
             ))}
           </select>
         </div>
         <button
-          type="submit"
-          className="rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800
-                               text-white font-bold w-1/2 mx-auto py-2 flex justify-center"
+          type='submit'
+          className='rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800
+                               text-white font-bold w-1/2 mx-auto py-2 flex justify-center'
           disabled={carregandoCategoria}
         >
-          {isLoading ? (
+          {isLoading ?
             <RotatingLines
               strokeColor="white"
               strokeWidth="5"
               animationDuration="0.75"
               width="24"
               visible={true}
-            />
-          ) : (
-            <span>{id !== undefined ? "Atualizar" : "Cadastrar"}</span>
-          )}
+            /> :
+            <span>{id !== undefined ? 'Atualizar' : 'Cadastrar'}</span>
+          }
         </button>
       </form>
     </div>
