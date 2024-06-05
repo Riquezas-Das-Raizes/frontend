d7ba017949b2190658ea44683ab79dabe86b386d
Lorena Belo <loore.belo@gmail.com>
2024-06-05 18:22:07 -0300

    fix: ajustes de merge

--- src/components/products/modalproducts/ModalProducts.tsx
+++ src/components/products/modalproducts/ModalProducts.tsx
@@ -1,8 +1,8 @@
 import Popup from "reactjs-popup";
-
 import "reactjs-popup/dist/index.css";
 import "./ModalProducts.css";
 import FormProduct from "../formproducts/FormProducts";
+
 
 function ModalProduct() {
   return (
@@ -10,25 +10,27 @@ function ModalProduct() {
       <Popup
         trigger={
           <button
-            className=" rounded
-                        font-bold
-                        text-custom-emerald 
-                        border-custom-green
-                        hover:bg-custom-emerald
-                        hover:text-white
-                        border-solid 
-                        border-2
-                        py-2 
-                        px-4'>
-                        Cadastrar Produto
-                    </button>
-                }
-                modal
-            >
-                <FormProduct />
-            </Popup>
-        </>
-    );
+            className="
+              rounded
+              font-bold
+              text-custom-emerald 
+              border-custom-green
+              hover:bg-custom-emerald
+              hover:text-white
+              border-solid 
+              border-2
+              py-2 
+              px-4
+            ">
+            Cadastrar Produto
+          </button>
+        }
+        modal
+      >
+        <FormProduct />
+      </Popup>
+    </>
+  );
 }
 
 export default ModalProduct;
