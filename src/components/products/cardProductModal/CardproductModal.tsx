78c0fe637364c6a5b3702f1f2b3174dfbd5049c4
Lorena Belo <loore.belo@gmail.com>
2024-06-06 11:23:42 -0300

    fix: ajustes nos cards de produtos e modal de produtos

--- src/components/products/cardProductModal/CardproductModal.tsx
+++ src/components/products/cardProductModal/CardproductModal.tsx
@@ -17,37 +17,34 @@ function CardProductModal({ product }: CardProductProps) {
       }
       modal
     >
-      <div className="flex p-4 bg-custom-beige rounded-2xl">
-        <div className="flex flex-col p-5">
-          <h1 className="text-xl text-white font-bold text-center mb-5 bg-custom-green rounded-xl">{product.nome}</h1>
-
+      <div className="flex flex-col md:flex-row p-4 bg-custom-beige rounded-2xl">
+        <div className="md:w-1/2 p-5 flex flex-col items-center">
+          <h1 className="text-xl text-white font-bold text-center mb-5 bg-custom-green rounded-xl p-2">{product.nome}</h1>
           <img
             src={product.imagem}
             alt={product.nome}
-            className="w-full object-cover rounded-xl shadow-2xl shadow-slate-500"
+            className="w-full h-72 object-cover rounded-xl mb-4 shadow-2xl shadow-slate-500"
           />
         </div>
-        <div className=" flex flex-col justify-center align-center w-full">
-          <p className="text-black text-xl text-center ">{product.descricao}</p>
-          <div className="border border-gray-300 mt-5"></div>
-          <div className="flex justify-center gap-x-11 mt-10 mb-5">
-            <p className="text-gray-900 font-bold text-2xl ">
+        <div className="md:w-1/2 p-5 flex flex-col justify-center">
+          <p className="text-black text-xl text-center mb-4">{product.descricao}</p>
+          <div className="border border-gray-300 my-4"></div>
+          <div className="flex justify-center items-center gap-x-5 mb-5">
+            <p className="text-gray-900 font-bold text-2xl">
               R$ {product.preco}
             </p>
             <button className="bg-custom-green hover:bg-custom-emerald text-white font-bold py-2 px-4 rounded-xl">
               Comprar
             </button>
           </div>
-          <div className="border border-gray-300"></div>
-          <div className="flex flex-col mt-5">
-            <div className="flex justify-center gap-x-5">
+          <div className="border border-gray-300 my-4"></div>
+          <div className="flex justify-center gap-x-5 mt-5">
             <a href="" target="_blank" className="transform transition-transform duration-300 hover:scale-110 hover:text-custom-red">
               <InstagramLogo size={36} />
             </a>
             <a href="" target="_blank" className="transform transition-transform duration-300 hover:scale-110 hover:text-custom-green">
               <WhatsappLogo size={36} />
             </a>
-            </div>
           </div>
         </div>
       </div>
--- src/components/products/cardproducts/CardProducts.tsx
+++ src/components/products/cardproducts/CardProducts.tsx
@@ -10,51 +10,53 @@ interface CardProductProps {
 
 function CardProducts({ product }: CardProductProps) {
   const [mostrar, setMostrar] = useState(false);
-
   const { usuario } = useContext(AuthContext);
 
   return (
     <div
-      className="max-w-sm w-full overflow-hidden shadow-lg bg-white transition-transform hover:scale-105 relative rounded-lg m-4"
+      className="max-w-sm w-full overflow-hidden shadow-lg bg-white transition-transform hover:scale-105 relative rounded-lg m-4 flex flex-col"
       onMouseEnter={() => setMostrar(true)}
       onMouseLeave={() => setMostrar(false)}
     >
-      <div className="relative p-4">
+      <div className="p-4 flex flex-col flex-grow relative">
         <img
-          className="w-full h-auto rounded-sm mb-4"
+          className="w-full h-72 object-cover rounded-sm mb-4"
           src={product.imagem}
           alt={product.nome}
         />
-        <div className="font-bold text-xl text-center mb-2">
-          {product.nome}
+        <div className="flex flex-col flex-grow">
+          <div className="font-bold text-xl text-center mb-2">
+            {product.nome}
+          </div>
+          <div className="text-center text-gray-700 text-base mb-2">
+            R$ {product.preco}
+          </div>
         </div>
-        <div className="text-center text-gray-700 text-base mb-2">
-          R$ {product.preco}
-        </div>
+        {usuario.admin && (
+          <div
+            className={`transition-opacity duration-300 ${
+              mostrar ? "opacity-100" : "opacity-0"
+            } absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-10 pointer-events-none ${
+              mostrar ? "pointer-events-auto" : "pointer-events-none"
+            }`}
+          >
+            <Link to={`/editarProduto/${product.id}`}>
+              <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2">
+                Editar
+              </button>
+            </Link>
+
+            <Link to={`/deletarProduto/${product.id}`}>
+              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                Apagar
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
       <div className="px-6 py-4 bg-gray-100 text-center">
         <CardProductModal product={product} />
       </div>
-
-      {usuario.admin && (
-        <div
-          className={`transition-opacity duration-300 ${
-            mostrar ? "opacity-100" : "opacity-0"
-          } absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-10`}
-        >
-          <Link to={`/editarProduto/${product.id}`}>
-            <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2">
-              Editar
-            </button>
-          </Link>
-
-          <Link to={`/deletarProduto/${product.id}`}>
-            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-              Apagar
-            </button>
-          </Link>
-        </div>
-      )}
     </div>
   );
 }
