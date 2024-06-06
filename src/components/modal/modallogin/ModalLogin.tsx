c6c9715920dc524f42aa4fad1273b7ada77b2e93
Lorena Belo <loore.belo@gmail.com>
2024-06-06 15:40:10 -0300

    Fix: ajustes no deploy

--- src/components/modal/modallogin/ModalLogin.tsx
+++ src/components/modal/modallogin/ModalLogin.tsx
@@ -1,22 +1,16 @@
 import Popup from "reactjs-popup";
-
-
 import 'reactjs-popup/dist/index.css';
-import './ModalLogin.css' 
-
+import './ModalLogin.css';
 import Login from "../../../pages/login/Login";
 
-function ModalLogin({triggerElement, titleElement}) {
-    return (
-        <>
-            <Popup 
-                trigger={triggerElement, titleElement}
-               modal
-            >
-                <Login />
-            </Popup>
-        </>
-    );
+function ModalLogin({ triggerElement }) {
+  return (
+    <>
+      <Popup trigger={triggerElement} modal>
+        <Login />
+      </Popup>
+    </>
+  );
 }
 
 export default ModalLogin;
