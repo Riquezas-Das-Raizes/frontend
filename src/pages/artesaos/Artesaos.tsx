f23c882a4c0118110fa4704176e07eec34554e41
Natasha <natasha.sousa.morais@gmail.com>
2024-06-22 17:21:21 -0300

    fix: troca img artesaos e exclui pasta assets

--- src/pages/artesaos/Artesaos.tsx
+++ src/pages/artesaos/Artesaos.tsx
@@ -23,7 +23,7 @@ function Artesaos() {
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-center gap-8 text-white">
           <div className="bg-custom-green rounded-lg shadow-md p-6 my-6 text-center w-full md:w-1/2 lg:w-1/4">
-            <img src="https://ik.imagekit.io/4lf7m2y5a/N%C3%B4made%20e%20Patax%C3%B3/produto_2_patax%C3%B3.jpg?updatedAt=1717598654344.jpg" alt="" className="rounded-full mx-auto h-48 w-48" />
+            <img src="https://ik.imagekit.io/natashadev/Riqueza%20das%20Ra%C3%ADzes/ED2EF.webp?updatedAt=1719080084710" alt="" className="rounded-full mx-auto h-48 w-48" />
             <h3 className="text-xl font-bold my-2">NÔMADE E PATAXÓ</h3>
             <p className="mb-0">Desde 2013, uma família formada por uma nômade e um indígena Pataxó, baseada em São Paulo - SP, trabalha 
               com miçangas de vidro jablonex, criando peças inspiradas nos povos originários da terra, feitas à mão 
@@ -31,14 +31,14 @@ function Artesaos() {
               <div className="justify-center flex gap-2 pt-3 text-custom-black hover:underline"><Browser size={25} /> <a href="https://www.elo7.com.br/nomadeepataxo" target="_blank" rel="noopener noreferrer">Conheça mais desse artesão</a></div>
           </div>
           <div className="bg-custom-green rounded-lg shadow-md p-6 my-6 text-center w-full md:w-1/2 lg:w-1/4">
-            <img src="https://ik.imagekit.io/4lf7m2y5a/Huluki%20Arte%20Ind%C3%ADgena/produto_9.jpeg?updatedAt=1717597214751.jpg.jpg" alt="" className="rounded-full mx-auto h-48 w-48" />
+            <img src="https://ik.imagekit.io/natashadev/Riqueza%20das%20Ra%C3%ADzes/819B56.webp?updatedAt=1719080053188" alt="" className="rounded-full mx-auto h-48 w-48" />
             <h3 className="text-xl font-bold my-2">HULUKI ARTE INDIGENA</h3>
             <p className="mb-0">Desde 2018, a Huluki, com sede em São Paulo - SP, promove a venda de arte indígena autêntica, gerando renda 
               para comunidades artesãs e oferecendo aos clientes uma experiência enriquecedora e satisfatória.</p>
               <div className="justify-center flex gap-2 p-3 text-custom-black hover:underline"><Browser size={25} /> <a href="https://www.elo7.com.br/hulukiarteindigena" target="_blank" rel="noopener noreferrer">Conheça mais desse artesão</a></div>              
           </div>
           <div className="bg-custom-green rounded-lg shadow-md p-6 my-6 text-center w-full md:w-1/2 lg:w-1/4">
-            <img src="https://ik.imagekit.io/natashadev/Riqueza%20das%20Ra%C3%ADzes/Imagem%20do%20WhatsApp%20de%202024-06-05%20%C3%A0(s)%2015.37.30_eea2be1b.jpg?updatedAt=1717689487761" alt="" className="rounded-full mx-auto h-48 w-48" />
+            <img src="https://ik.imagekit.io/natashadev/Riqueza%20das%20Ra%C3%ADzes/DE3DDE.jpg?updatedAt=1719080075655" alt="" className="rounded-full mx-auto h-48 w-48" />
             <h3 className="text-xl font-bold my-2">AWATXUHU ARTESANATO </h3>
             <p className="mb-0">Aqui você vai encontrar peças produzidas manualmente pelas artesãs da Comunidade Indígena Puyanawa.</p>
               <div className="justify-center flex gap-2 pt-3 text-custom-black hover:underline"><Browser size={25} /> <a href="https://www.elo7.com.br/AwatxuhuArtesanatosPuyanawa" target="_blank" rel="noopener noreferrer">Conheça mais desse artesão</a></div>
