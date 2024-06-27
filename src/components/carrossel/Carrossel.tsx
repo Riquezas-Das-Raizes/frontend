3dd569d78f338bcdbec94acecd83d375a2b3d86f
Lorena Belo <loore.belo@gmail.com>
2024-06-27 16:00:27 -0300

    fix: ajuste na imagem do slide

--- src/components/carrossel/Carrossel.tsx
+++ src/components/carrossel/Carrossel.tsx
@@ -43,28 +43,18 @@ import { Link } from "react-router-dom";
                     <Link to="/categorias/14">
                 <img
                         className="swiper-slide-img" 
-                        src="https://ik.imagekit.io/z5xvlplbb/New%20Folder/Conectando...png" 
+                        src="https://ik.imagekit.io/z5xvlplbb/New%20Folder/Conectando.png" 
                         alt="Carrossel - Slide 01 Conectando Tradição e  Modernidade em Cada Compra" 
                     />
                     </Link>
                 </SwiperSlide>
 
-                {/* <SwiperSlide>
+                <SwiperSlide>
                     <Link to="/artesaos">
                     <img
                         className="swiper-slide-img" 
                         src="https://ik.imagekit.io/natashadev/Riqueza%20das%20Ra%C3%ADzes/Green%20Minimalist%20Nature%20Quotes%20Desktop%20Wallpaper%20(3).png?updatedAt=1717678702590" 
                         alt="Carrossel - Slide 02 Todos os produtos apresentados nesse trabalho são de artesãos reais e você pode conferir o trabalho deles na nossa página artesãos!" 
-                    />
-                    </Link>
-                </SwiperSlide> */}
-
-                <SwiperSlide>
-                    <Link to="/artesaos">
-                    <img
-                        className="swiper-slide-img" 
-                        src="https://ik.imagekit.io/z5xvlplbb/New%20Folder/slide2.png?updatedAt=1719506221410" 
-                        alt="Carrossel - Slide 02 Confira quem são nossos artesões responsáveis por cada peça." 
                     />
                     </Link>
                 </SwiperSlide>
