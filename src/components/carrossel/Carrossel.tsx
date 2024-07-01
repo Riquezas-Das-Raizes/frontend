ca2240a06690381ece08555dc21cd1b1c143e0df
Natasha <natasha.sousa.morais@gmail.com>
2024-07-01 18:47:26 -0300

    fix:atualiza imagens do carrossel  e do mobile

--- src/components/carrossel/Carrossel.tsx
+++ src/components/carrossel/Carrossel.tsx
@@ -28,16 +28,6 @@ import { Link } from "react-router-dom";
                 className="mySwiper"
             >
 
-                {/* <SwiperSlide>
-                    <Link to="/categorias/14">
-                <img
-                        className="swiper-slide-img" 
-                        src="https://ik.imagekit.io/natashadev/Riqueza%20das%20Ra%C3%ADzes/Green%20Minimalist%20Nature%20Quotes%20Desktop%20Wallpaper%20(1).png?updatedAt=1717678702730" 
-                        alt="Carrossel - Slide 01 Conectando Tradição e  Modernidade em Cada Compra
-                        " 
-                    />
-                    </Link>
-                </SwiperSlide> */}
 
                 <SwiperSlide>
                     <Link to="/categorias/14">
@@ -53,11 +43,22 @@ import { Link } from "react-router-dom";
                     <Link to="/artesaos">
                     <img
                         className="swiper-slide-img" 
-                        src="https://ik.imagekit.io/natashadev/Riqueza%20das%20Ra%C3%ADzes/Green%20Minimalist%20Nature%20Quotes%20Desktop%20Wallpaper%20(3).png?updatedAt=1717678702590" 
+                        src="https://ik.imagekit.io/natashadev/Riqueza%20das%20Ra%C3%ADzes/Confira%20quem%20s%C3%A3o%20os%20artes%C3%A3os%20respons%C3%A1veis%20por%20cada%20pe%C3%A7a/Confira%20quem%20s%C3%A3o%20os%20artes%C3%A3os%20respons%C3%A1veis%20por%20cada%20pe%C3%A7a%20(2).svg?updatedAt=1719866061966" 
                         alt="Carrossel - Slide 02 Todos os produtos apresentados nesse trabalho são de artesãos reais e você pode conferir o trabalho deles na nossa página artesãos!" 
                     />
                     </Link>
                 </SwiperSlide>
+
+                
+                <SwiperSlide>
+                    <Link to="/categorias/14">
+                <img
+                        className="swiper-slide-img" 
+                        src="https://ik.imagekit.io/natashadev/Riqueza%20das%20Ra%C3%ADzes/Confira%20quem%20s%C3%A3o%20os%20artes%C3%A3os%20respons%C3%A1veis%20por%20cada%20pe%C3%A7a/Confira%20quem%20s%C3%A3o%20os%20artes%C3%A3os%20respons%C3%A1veis%20por%20cada%20pe%C3%A7a%20(3).svg?updatedAt=1719867216450" 
+                        alt="Carrossel - Slide 03 Aqui você encontra artesanato indígena autêntico e colabora com o desenvolvimento sustentável de comunidades tradicionais!" 
+                    />
+                    </Link>
+                </SwiperSlide> 
 
             </Swiper>
             </div>
